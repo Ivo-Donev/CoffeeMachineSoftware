bacb9247fa44f37b55981343506bfbd3a7b3688d
Ivo Donev <153001406+Ivo-Donev@users.noreply.github.com>
2023-12-14 00:37:18 +0100

    OOP Version

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
 is_on = True
-
 while is_on:
     options = menu.get_items()
     choice = input(f"What would you like? ({options}): ")
